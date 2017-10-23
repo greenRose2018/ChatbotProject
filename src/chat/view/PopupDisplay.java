4685af7ed5ae346d7db81d99bd2e4b29bcebb1a1
greenRose2018 <brifun13@gmail.com>
2017-10-23 08:46:07 -0600

    added popupdisplay code to interact with the user, intialized datamembers, added pic

--- src/chat/view/PopupDisplay.java
+++ src/chat/view/PopupDisplay.java
@@ -1,9 +1,28 @@
 package chat.view;
 
-//import javax.swing.JOptionPane;
-
+import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
 public class PopupDisplay
 {
+	private ImageIcon icon;
+	private String windowTitle;
 	
+	public PopupDisplay()
+	{
+		icon = new ImageIcon(getClass().getResource("images/bubble.jpg"));
+		windowTitle = "Sample";
+	}
+	
+	public void displayText(String message)
+	{
+		JOptionPane.showMessageDialog(null, message, windowTitle, JOptionPane.INFORMATION_MESSAGE, icon);
+	}
+	
+	public String getResponse(String question)
+	{
+		String answer ="";
+		answer += JOptionPane.showInputDialog(null, question, windowTitle, JOptionPane.PLAIN_MESSAGE, icon, null,"");
+		return answer;
+	}
 
 }
