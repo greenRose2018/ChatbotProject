3d0fd5d3a1e9c5a5f53b0567a0ea14c501ca7855
greenRose2018 <brifun13@gmail.com>
2017-11-01 09:59:22 -0600

    added code in controller and edited popupDisplay

--- src/chat/controller/ChatbotController.java
+++ src/chat/controller/ChatbotController.java
@@ -1,17 +1,50 @@
 package chat.controller;
 
+import chat.model.Chatbot;
+import chat.model.Movie;
+import chat.view.PopupDisplay;
+import chat.view.ChatFrame;
+import chat.view.ChatPanel;
+
 public class ChatbotController
 {
-	//private PopupDisplay;
-	//private Chatbot;
+	private PopupDisplay display;
+	private Chatbot chatbot;
 	
 	public ChatbotController()
 	{
-		
+		chatbot = new Chatbot("Brittney Morales");
+		display = new PopupDisplay();
 	}
 	
 	public void start()
 	{
+		String response = display.collectResponse("What do you want to talk about?");
 		
+		while (chatbot.lengthChecker(response) && !chatbot.quitChecker(response));
+		{
+			response = popupChat(response);
+			response = display.collectResponse(response);
+		}
 	}
+	
+	public String interactWithChatbot(String input)
+	{
+		return null;
+	}
+	
+	private String popupChat(String chat)
+	{
+		String chatbotSays = "";
+		
+		chatbotSays += chatbot.processConversation(chat);
+		
+		return chatbotSays;
+	}
+	
+	public Chatbot getChatbot()
+	{
+		return null;
+	}
+	
 }
