ba994532605fc7d18883b01870524b9d3a515a46
greenRose2018 <brifun13@gmail.com>
2017-11-13 09:50:13 -0700

    added JFrame to ChatFrame (setup)

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -1,6 +1,22 @@
 package chat.view;
 
-public class ChatFrame
-{
+import chat.controller.ChatbotController;
+import javax.swing.JFrame;
 
+public class ChatFrame extends JFrame
+{
+	private ChatbotController appController;
+	
+	public ChatFrame(ChatbotController appController)
+	{
+		super();
+		this.appController = appController;
+		
+		setupFrame();
+	}
+
+	private void setupFrame()
+	{
+		this.setVisible(true);
+	}
 }
--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,8 +1,5 @@
 package chat.view;
 
-import chat.controller.ChatbotController;
-import javax.swing.JFrame;
-
 public class ChatPanel
 {
 
