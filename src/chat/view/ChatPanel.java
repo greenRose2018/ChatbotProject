cd73f2d083d0e8deb87fc2b461f72a34bfca7082
greenRose2018 <brifun13@gmail.com>
2017-11-15 08:59:36 -0700

    added imports

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -2,5 +2,12 @@ package chat.view;
 
 public class ChatPanel
 {
+import chat.controller.ChatbotController;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.SpringLayout;
+import javax.swing.JComponent;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 }
