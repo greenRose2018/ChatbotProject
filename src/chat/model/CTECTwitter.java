d5432c9428502bf445d8e6e538ecc37c4a35e343
greenRose2018 <brifun13@gmail.com>
2018-02-21 09:30:32 -0700

    added code to sendTweet

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -21,7 +21,18 @@ public class CTECTwitter
 	
 	public void sendTweet(String textToTweet)
 	{
-		
+		try
+		{
+			chatbotTwitter.updateStatus(textToTweet + "@ChatbotCTEC");
+		}
+		catch(TwitterException tweetError)
+		{
+			appController.handleErrors(tweetError);
+		}
+		catch(Exception otherError)
+		{
+			appController.handleErrors(otherError);
+		}
 	}
 	
 //	private String[] createIgnoredWordArray()
