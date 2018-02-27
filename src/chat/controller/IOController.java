4d493dde57acd6240d0a4379c83c00a4cf87d2b4
greenRose2018 <brifun13@gmail.com>
2018-02-27 08:30:49 -0700

    created file IOController, basic setup plus imports

---
+++ src/chat/controller/IOController.java
@@ -0,0 +1,11 @@
+package chat.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class IOController
+{
+}
