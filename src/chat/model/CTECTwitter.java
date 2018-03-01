7569755ce4ee31e56a42b66d094980a247051fba
greenRose2018 <brifun13@gmail.com>
2018-03-01 07:43:54 -0700

    added imports and new data members

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -1,16 +1,28 @@
 package chat.model;
 
 import chat.controller.ChatbotController;
-
+import chat.controller.IOController;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.Twitter;
+import twitter4j.GeoLocation;
+import twitter4j.Paging;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.ResponseList;
 import twitter4j.Status;
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.text.DecimalFormat;
 
 public class CTECTwitter
 {
 	private ChatbotController appController;
 	private Twitter chatbotTwitter;
+	private List<Status> searchedTweeets;
+	private List<String> weetedWords;
 	
 	public CTECTwitter(ChatbotController appController)
 	{
